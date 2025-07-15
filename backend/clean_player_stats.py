0b7e7cbc9e19253f054863e2bd27849c71f2e92a
pmaddula4 <pranavmaddula36@gmail.com>
2025-07-15 15:54:37 -0500

    changed sample data to rounded figures

--- backend/clean_player_stats.py
+++ backend/clean_player_stats.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv("transfer_portal_all_stats_2025_full.csv")
+df = pd.read_csv("data/transfer_portal_all_stats_2025_full.csv")
 
 df.rename(columns={
     "unknown1": "defensiveRating",
@@ -28,11 +28,14 @@ del df["2ptnums"]
 del df["3ptnums"]
 
 df['minutes'] = df['min_pct'] * 0.4
+df['minutes'] = df['minutes'].round(2)
 df['name'] = df['name'].str.replace(r'\*$', '', regex=True)
 df['height'] = df['height'].str.replace('-', "'", regex=False)
 df['height'] = df['height'] + '"'
 df['threePtPercent'] = df['3pt_pct'] * 100
+df['threePtPercent'] = df['threePtPercent'].round(1)
 df['ftPercent'] = df['ft_pct'] * 100
+df['ftPercent'] = df['ftPercent'].round(1)
 
-df.to_csv("cleaned_player_stats_full.csv", index=False)
+df.to_csv("data/cleaned_player_stats_full.csv", index=False)
 print("cleaned player stats saved as 'cleaned_player_stats_full.csv'")
